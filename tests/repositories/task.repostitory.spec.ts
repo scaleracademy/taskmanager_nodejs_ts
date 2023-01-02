cfdee28c8046859cc8c28bef745d065e548f855e
Arnav Gupta <championswimmer@gmail.com>
2023-01-02 16:25:39 +0530

    add tests for entities and repositories
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- tests/repositories/task.repostitory.spec.ts
+++ tests/repositories/task.repostitory.spec.ts
@@ -1,9 +1,22 @@
 import { expect } from 'chai';
 import { taskRepo } from '../../src/repositories/task.repository';
+import { Task } from 'src/entities/task.entity';
 
 
 describe("Task Repository", () => {
   it('should have 0 tasks in the beginning', () => {
     expect(taskRepo.getAllTasks()).to.have.lengthOf(0);
+  })
+
+  it('should create a new task', () => {
+    const task = taskRepo.createTask('Test Task', 'This is a test task', '2021-01-01');
+    expect(task).to.be.instanceOf(Task)
+    expect(task.id).to.equal(1);
+    expect(task.title).to.equal('Test Task');
+    expect(task.description).to.equal('This is a test task');
+    expect(task.deadline.getTime()).to.equal(new Date('2021-01-01').getTime());
+    expect(task.completed).to.be.false;
+
+    expect(taskRepo.getAllTasks()).to.have.lengthOf(1);
   })
 });
